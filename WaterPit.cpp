120bb424cee8db0defa0cf37430b2287cee97992
yahyaosama <yahyaosamamohamedlotfy@gmail.com>
2026-04-27 22:46:10 +0300

    execute water pit

--- WaterPit.cpp
+++ WaterPit.cpp
@@ -1,31 +1,42 @@
 #include "WaterPit.h"
+#include "GameState.h"
+#include "Player.h"
 
+WaterPit::WaterPit(const CellPosition &waterPitPosition)
+    : GameObject(waterPitPosition) {}
 
+void WaterPit::Draw(Output *pOut) const { pOut->DrawWaterPit(position); }
 
-WaterPit::WaterPit(const CellPosition & waterPitPosition):GameObject(waterPitPosition)
-{
+void WaterPit::Apply(Grid *pGrid, GameState *pState, Player *pPlayer) {
+
+  /// TODO: Implement this function as mentioned in the guideline steps
+  /// (numbered below) below
+
+  // == Here are some guideline steps (numbered below) to implement this
+  // function ==
+
+  // 1- Print a message, make sure to edit this message according to which
+  // player wins "You drowned in a water pit. Player (0/1) wins !  Click to
+  // continue ..." and wait mouse click
+  int winnerNum = -1;
+  for (int i = 0; i < MaxPlayerCount; i++)
+  {
+    if (pState->GetPlayer(i) != pPlayer)
+    {
+      winnerNum = i;
+      break;
+    }
+  }
+  string msg = "You drowned in a water pit. Player " + to_string(winnerNum) + " wins!  Click to continue ...";
+  pGrid->PrintErrorMessage(msg);
+
+  // 2- Apply the water pit's effect by setting the player's health which
+  // drowned to zero and ending the game
+  pPlayer->SetHealth(0);
+  pState->SetEndGame(true);
+
+  // 3- Update the players info which is displayed (check Grid class and decide which function to use)
+  pGrid->UpdateInterface(pState);
 }
 
-void WaterPit::Draw(Output * pOut) const
-{
-	pOut->DrawWaterPit(position);
-}
-
-void WaterPit::Apply(Grid* pGrid, GameState* pState, Player* pPlayer)
-{
-
-	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
-
-
-	// == Here are some guideline steps (numbered below) to implement this function ==
-
-	// 1- Print a message, make sure to edit this message according to which player wins "You drowned in a water pit. Player (0/1) wins !  Click to continue ..." and wait mouse click
-
-	// 2- Apply the water pit's effect by setting the player's health which drowned to zero and ending the game
-	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
-}
-
-
-WaterPit::~WaterPit()
-{
-}
+WaterPit::~WaterPit() {}
