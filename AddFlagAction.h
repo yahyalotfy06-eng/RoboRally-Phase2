2470cb4814faf7fc3d9b6ab8cbd26e4b9f49026f
hala elgammal <hala.mahmoud07@eng-st.cu.edu.eg>
2026-04-30 15:43:39 +0300

    Add Flag Action completed

--- AddFlagAction.h
+++ AddFlagAction.h
@@ -9,10 +9,15 @@ class AddFlagAction :public Action
 
 	// [Action Parameters]
 	CellPosition flagPos; // 1- The position of the flag
+
+	static bool isFlagAdded;
+
 public:
 	
 	AddFlagAction(ApplicationManager *pApp);
 	virtual void ReadActionParameters(); // Reads action parameters 
+
+	static void resetisflag();
 
 	virtual void Execute(); // Creates a new flag Object 
 							// then Sets this flag object to the GameObject Pointer of its Cell
